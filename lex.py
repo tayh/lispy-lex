f2a618f17ee5a4431f927e1c1a72ff97b81511a7
Taynara Carvalho <tayhcarvalho@gmail.com>
2020-10-13 10:59:55 -0300

    Create NUMBER type

--- lex.py
+++ lex.py
@@ -6,6 +6,10 @@ class Token(NamedTuple):
     kind: str
     value: str
 
+def tokenize(chars: str) -> list:
+    "Convert a string of characters into a list of tokens."
+    return chars.replace('(', ' ( ').replace(')', ' ) ').split()
+
 def lex(code: str) -> Iterable[Token]:
     """
     Retorna sequência de objetos do tipo token correspondendo à análise léxica
@@ -16,21 +20,20 @@ def lex(code: str) -> Iterable[Token]:
         ('NAME', r"[a-zA-Z_][a-zA-Z_0-9]*"),
         ('LPAR', r'\('),
         ('RPAR', r'\)'),
+        ('NUMBER', r'\+?\-?\d+(\.\d*)?([eE][+-]?\d+)?'),
     ]
     tok_regex = '|'.join('(?P<%s>%s)' % pair for pair in token_specification)
-    print(tok_regex)
     for mo in re.finditer(tok_regex, code):
         kind = mo.lastgroup
         value = mo.group()
         yield Token(kind, value)
 
+exemplos = [
+    "1 2.0 -1 3.14 42.0 +100"
+]
 
-# exemplos = [
-#     "(max x y)"
-# ]
-
-# for ex in exemplos:
-#     print(ex)
-#     for tok in lex(ex):
-#         print('    ', tok)
-#     print()
+for ex in exemplos:
+    print(ex.split())
+    for tok in lex(ex):
+        print('    ', tok)
+    print()
